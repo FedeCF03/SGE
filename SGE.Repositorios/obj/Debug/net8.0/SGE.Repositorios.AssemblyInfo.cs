79811fe9abe465e4392d768a2fe8562172f3cfde
FedeCF03 <fedecf03@gmail.com>
2024-05-10 13:18:00 -0300

    S

--- SGE.Repositorios/obj/Debug/net8.0/SGE.Repositorios.AssemblyInfo.cs
+++ SGE.Repositorios/obj/Debug/net8.0/SGE.Repositorios.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("SGE.Repositorios")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5bcff66d7511820b97e1fdd10531d1d3fb88ab17")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e3bd2079539ca291c1c9bff465fbf4cf9a9854f7")]
 [assembly: System.Reflection.AssemblyProductAttribute("SGE.Repositorios")]
 [assembly: System.Reflection.AssemblyTitleAttribute("SGE.Repositorios")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
